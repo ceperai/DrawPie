9aa21bed0ec9e082d5f1c38e8b3de6e4221b889f
Sergey Pestov <pestov@smartdriving.io>
2016-04-25 14:54:20 +0300

    Initial Commit

---
+++ DrawPie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawPie
+//
+//  Created by Sergey Pestov on 25/04/16.
+//  Copyright © 2016 Sergey Pestov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
